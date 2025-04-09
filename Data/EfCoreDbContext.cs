fe0c24b2d9b799f2c717812d77bddc01bdde67c1
saurav4762 <upretysaurav4@gamil.com>
2025-04-09 14:28:40 +0545

    Data Added

---
+++ Data/EfCoreDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskTrackr.Entities;
+
+namespace TaskTrackr.Data
+{
+    public class EfCoreDbContext : DbContext
+    {
+        public EfCoreDbContext(DbContextOptions<EfCoreDbContext> options) : base(options)
+        {
+        }
+        
+        public DbSet<Assignment> Assignments { get; set; }
+        
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Student>().ToTable("Students");
+            modelBuilder.Entity<Assignment>().ToTable("Assignments");
+        }
+
+    }
+}
