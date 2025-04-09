9888599300741584d376fe2a12af50ed313cc93f
saurav4762 <upretysaurav4@gamil.com>
2025-04-09 14:23:06 +0545

    Added Entities

---
+++ Entities/Assignment.cs
@@ -0,0 +1,12 @@
+﻿namespace TaskTrackr.Entities;
+
+public class Assignment
+{
+    public int Id { get; set; }
+    
+    public string AssignmentName { get; set; }
+    
+    public string AssignmentDescription { get; set; }
+    
+    public DateTime AssignmentDate { get; set; }
+}
---
+++ Entities/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskTrackr.Entities;
+
+public class Student
+{
+    public int Id { get; set; }
+    
+    public string FullName { get; set; }
+    
+    public string Email { get; set; }
+}
